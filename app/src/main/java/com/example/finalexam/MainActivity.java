9bb85fa1980390cc9ea5ddd3a16b7cb0e144cce0
OmarChaar <oishaar@gmail.com>
2018-05-11 14:11:12 +0300

    added image from API

--- app/src/main/java/com/example/finalexam/MainActivity.java
+++ app/src/main/java/com/example/finalexam/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.finalexam;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.finalexam.api.ApiManager;
 import com.example.finalexam.models.WeatherInfo;
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 
@@ -24,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView_ArtistName;
     private TextView textView_SongName;
 
+    private ImageView imageView_SongTitle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         textView_ArtistName = findViewById(R.id.textView_ArtistName);
         textView_SongName = findViewById(R.id.textView_SongName);
 
+        imageView_SongTitle = findViewById(R.id.imageView_SongTitle);
 
         apiManager.getWeatherAtCoordinates().enqueue(new Callback() {
             @Override
@@ -50,6 +56,16 @@ public class MainActivity extends AppCompatActivity {
 
                     textView_ArtistName.setText("Artist Name: " + name);
                     textView_SongName.setText("Song Name: " + song);
+
+
+//                    Log.d("HERE", " " + imageURL);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String imageURL = artistName.getData().get(0).getImageURL();
+                            Picasso.with(MainActivity.this).load(imageURL).into(imageView_SongTitle);
+                        }
+                    });
 
                 }
             }
